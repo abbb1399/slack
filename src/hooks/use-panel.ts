5dac7869d34dfa64c0a3ea9a7b544fa9a280d042
paul <abbb123@naver.com>
2024-09-24 12:52:05 +0900

    feat: member profile panel

--- src/hooks/use-panel.ts
+++ src/hooks/use-panel.ts
@@ -1,19 +1,30 @@
 import { useParentMeesageId } from "@/features/messages/store/use-parent-meesage-id";
+import { useProfileMemberId } from "@/features/members/store/use-profile-member-id";
 
 export const usePanel = () => {
   const [parentMessageId, setParentMeesageId] = useParentMeesageId();
+  const [profileMemberId, setProfileMemberId] = useProfileMemberId();
+
+  const onOpenProfile = (memberId: string) => {
+    setProfileMemberId(memberId);
+    setParentMeesageId(null);
+  };
 
   const onOpenMessage = (messageId: string) => {
     setParentMeesageId(messageId);
+    setProfileMemberId(null);
   };
 
   const onClose = () => {
     setParentMeesageId(null);
+    setProfileMemberId(null);
   };
 
   return {
     parentMessageId,
+    profileMemberId,
     onOpenMessage,
+    onOpenProfile,
     onClose,
   };
 };
